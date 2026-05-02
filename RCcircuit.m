0734c29d59c73cb1d37e045c4d4a1e5f5305ae21
NicoleLaessle <1034390@my.scccd.edu>
2026-05-01 17:21:28 -0700

    Update RCcircuit.m

--- RCcircuit.m
+++ RCcircuit.m
@@ -1,1 +1,6 @@
+RC circuit application using ode45 solver
+Report the voltage after: 0.25, 0.5, and 1.0 seconds (give values to 2 decimal places)
+• How long does it take for the voltage to reach 8.0 Volts? (give answer to 2 decimal places)
+• Plot the voltage V(t) for the first one second. Set the range of the vertical axis from 0 to 10 V. Include
+a grid, title, and axis labels.
 
